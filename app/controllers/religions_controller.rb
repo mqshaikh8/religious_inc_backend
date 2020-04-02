5abeba89bc750d86466e1e290ea7c51cb77508bc
MohammedQasim <mqshaikh8@gmail.com>
2020-04-02 08:30:17 -0400

    started the editing feature

--- app/controllers/religions_controller.rb
+++ app/controllers/religions_controller.rb
@@ -14,6 +14,20 @@ class ReligionsController < ApplicationController
     end
 
     def create
+        religion = Religion.find_by(name:params[:religion])
+
+        category = Category.find_by(name:params[:category],religion_id:religion.id)
+        if category == nil
+        category = Category.create(name:params[:category],religion_id:religion.id)
+        end
+       
+        topic = Topic.create(name:params[:Topic],category_id:category.id)
+        
+
+        article = Article.create(content:params[:Content],topic_id:topic.id)
+        
+
+        render json: {Succes:article}
     end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ApplicationRecord
     belongs_to :topic
+
+    def self.articles
+        articleArr = {}
+        Article.all.each do |article|
+            articleArr[article.topic_id] = article.content
+        end
+        return articleArr
+    end
 end
